7568c917a7833dc7c1f4caf09ddf12083f3a2b52
smithamalur <smitha.malur@gmail.com>
2017-06-28 22:28:42 -0400

    feat(module-api): init project

---
+++ api/CarTrackerApi/src/main/java/me/smalur/App.java
@@ -0,0 +1,13 @@
+package me.smalur;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
